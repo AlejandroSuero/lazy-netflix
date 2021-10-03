602991cab5100c3509b8bf1029afba5256a8175e
Alejandro Suero <71392160+AlejandroSuero@users.noreply.github.com>
2021-10-03 20:06:55 +0200

    Updated to latest Netflix changes

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,7 @@
 const interval = setInterval(() => {
-    let skipIntro = document.querySelector('.skip-credits span');
-    let continueWatching = document.querySelector('.interrupter-actions');
+    //let skipIntro = document.querySelector('.skip-credits span');
+    let continueWatching = document.querySelector('button[data-uia]');
 
     if (skipIntro != null) skipIntro.click();
-    if (continueWatching != null) continueWatching.children[0].click();
+    if (continueWatching != null) continueWatching.click();
 }, 1000);
