02647eafb0b71b30034fc9ff19bff308153ae2c1
Alejandro Suero <AlejandroSuero@users.noreply.github.com>
2021-11-17 15:57:40 +0100

    Uncommented

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,7 @@
 const interval = setInterval(() => {
-    //let skipIntro = document.querySelector('.skip-credits span');
-    let continueWatching = document.querySelector('button[data-uia]');
+	let skipIntro = document.querySelector('.skip-credits span');
+	let continueWatching = document.querySelector('button[data-uia]');
 
-    if (skipIntro != null) skipIntro.click();
-    if (continueWatching != null) continueWatching.click();
+	if (skipIntro != null) skipIntro.click();
+	if (continueWatching != null) continueWatching.click();
 }, 1000);
